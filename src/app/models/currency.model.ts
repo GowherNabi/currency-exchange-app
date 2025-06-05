ff837bdd7a8a3000f5924dfb54d878abe94c39ac
Gowher Dar <gowher.dar@expentor.com>
2025-06-05 14:01:16 +0530

    currency model updated

--- src/app/models/currency.model.ts
+++ src/app/models/currency.model.ts
@@ -2,8 +2,3 @@ export interface Currency {
   code: string;
   name: string;
 }
-
-export interface ExchangeRateResponse {
-  date: string;
-  [currency: string]: unknown; // Can be number or nested object depending on API version
-}
