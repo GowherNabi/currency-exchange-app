f8bdf2653cb6712ffb7dbb0ade9e09f6fd9648ad
Gowher Dar <gowher.dar@expentor.com>
2025-06-06 11:47:53 +0530

    unit test removed from model

--- src/app/models/currency.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CurrencyModel } from './currency.model';
-
-describe('CurrencyModel', () => {
-  it('should create an instance', () => {
-    expect(new CurrencyModel()).toBeTruthy();
-  });
-});
--- src/app/models/exchange-rate.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ExchangeRateModel } from './exchange-rate.model';
-
-describe('ExchangeRateModel', () => {
-  it('should create an instance', () => {
-    expect(new ExchangeRateModel()).toBeTruthy();
-  });
-});
